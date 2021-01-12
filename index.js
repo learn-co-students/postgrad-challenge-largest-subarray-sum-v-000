97c2b836096277f29199468c12db54ec03666bbf
jamieberrier <berrier.jamie@gmail.com>
2021-01-12 09:20:59 -0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// Given an array of integers find a sequence with the largest sum.
+
+// Use it as a mechanism to focus on process. 
+// Think about this problem as a practice in breaking down problems, and knowing when to retreat from a potential solution.
+// A couple of leading questions that might help you get started:
+
+// What are some examples of input arrays where the solution becomes obvious? What can you learn from those obvious cases?
+// What would a naive solution look like, that tested every possible combination?
+
+// Challenge - Time Complexity
+// After you've implemented a basic solution, consider how many operations it will use to find an answer, based on the size of the input array. If you can, write down the Big O time complexity of your solution.
+
+// How could you make your solution faster? Try to improve your solution's runtime, or explain why you think your solution is as fast as possible.
+
+function largestSubarraySum(array){
+  
+}
